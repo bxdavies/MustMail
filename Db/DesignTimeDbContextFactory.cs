543988e687adc7a6c70cf24becff05245adf8bd7
ben <ben@daviesfamily.uk>
2026-05-05 20:15:50 +0100

    Add DesignTimeDbContextFactory.cs

---
+++ Db/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace MustMail.Db;
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        public DatabaseContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+
+            var provider = Environment.GetEnvironmentVariable("DB_PROVIDER");
+
+            if (provider == "Postgres")
+            {
+                optionsBuilder.UseNpgsql();
+            }
+            else
+            {
+                optionsBuilder.UseSqlite();
+            }
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
+    }
