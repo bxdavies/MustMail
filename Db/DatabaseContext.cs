b2a07e8e875c36445625a7a0f2d6bad399efeb53
ben <ben@daviesfamily.uk>
2026-05-05 20:15:31 +0100

    Switch DatabaseContext.cs to file scoped namespace

--- Db/DatabaseContext.cs
+++ Db/DatabaseContext.cs
@@ -1,11 +1,10 @@
-﻿namespace MustMail.Db
+﻿namespace MustMail.Db;
+
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 {
-    public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
-    {
-        public DbSet<User> User { get; set; } = null!;
+    public DbSet<User> User { get; set; } = null!;
 
-        public DbSet<SMTPAccount> SMTPAccount { get; set; } = null!;
+    public DbSet<SMTPAccount> SMTPAccount { get; set; } = null!;
 
-        public DbSet<Message> Message { get; set; } = null!;
-    }
+    public DbSet<Message> Message { get; set; } = null!;
 }
