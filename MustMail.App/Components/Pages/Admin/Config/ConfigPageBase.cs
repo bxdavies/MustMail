7954acd11fef764ef64ad74f6cc085277115c115
ben <ben@daviesfamily.uk>
2026-06-03 12:40:31 +0100

    Add unsaved changes warning

--- MustMail.App/Components/Pages/Admin/Config/ConfigPageBase.cs
+++ MustMail.App/Components/Pages/Admin/Config/ConfigPageBase.cs
@@ -1,4 +1,7 @@
+using KellermanSoftware.CompareNetObjects;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Graph.Models;
 using Microsoft.JSInterop;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -10,13 +13,46 @@ public partial class ConfigPageBase : ComponentBase
     protected MudForm? SettingsForm;
     protected Configuration Configuration = null!;
 
+    private IDisposable? registration;
+
     [Inject] public IJSRuntime JS { get; set; } = null!;
     [Inject] public ISnackbar Snackbar { get; set; } = null!;
     [Inject] public IConfiguration RawConfiguration { get; set; } = null!;
+    [Inject] public NavigationManager Navigation {  get; set; } = null!;
+    [Inject] public IDialogService DialogService { get; set; } = null!;
+
+    private async ValueTask OnLocationChanging(LocationChangingContext context)
+    {
+        CompareLogic compareLogic = new(new ComparisonConfig { MaxDifferences = 10 });
+        ComparisonResult result = compareLogic.Compare(Configuration, RawConfiguration.Get<Configuration>());
+
+        if (!result.AreEqual)
+        {
+            bool confirmed = await DialogService.ShowMessageBoxAsync(
+                "Unsaved Changes",
+                "You have unsaved changes that will be lost. Are you sure you want to leave this page?",
+                yesText: "Leave",
+                cancelText: "Stay") ?? false;
+
+            if (!confirmed)
+            {
+                context.PreventNavigation();
+            }
+        }
+    }
 
     protected override void OnInitialized()
     {
         Configuration = RawConfiguration.Get<Configuration>()!;
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            registration =
+                Navigation.RegisterLocationChangingHandler(OnLocationChanging);
+        }
     }
 
     protected async Task ValidateAndSave()
@@ -43,6 +79,8 @@ public partial class ConfigPageBase : ComponentBase
         await JS.InvokeVoidAsync("navigator.clipboard.writeText", value);
         _ = Snackbar.Add("Environment variable copied", Severity.Success);
     }
+
+    public void Dispose() => registration?.Dispose();
 
     [GeneratedRegex(@"^(\*|[A-Za-z0-9._%+-]+)@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")]
     protected static partial Regex AllowedEmailRegex();
